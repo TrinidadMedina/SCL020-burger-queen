9cb7eaace3673d61bd3809c9046ff7277d7ebae5
MacaParedes <maka.paredes@gmail.com>
2022-07-28 08:17:28 -0400

    clock

--- cafeteria/src/components/Table.jsx
+++ cafeteria/src/components/Table.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
 export const Table = ({ table }) => {
-    console.log(table.active)
     return (
         table.number && <>
-            <div className="py-2  max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-                <div className={table.active ? " max-w-sm mx-auto bg-cyan-600	 shadow-lg rounded-lg overflow-hidden" : " max-w-sm mx-auto bg-gray-300	 shadow-lg rounded-lg overflow-hidden"}>
-                    <h1 style={{ width: "20px" }}>#{table.number}</h1>
-                    <h2 > Check in Time:  {table.active ? table.checkInTime : " -"} </h2>
-                </div>
+            <div className={table.active ? " p-4 max-w-sm mx-auto bg-cyan-600	 shadow-lg rounded-lg overflow-hidden" : " p-4 max-w-sm mx-auto bg-gray-300	 shadow-lg rounded-lg overflow-hidden"}>
+                <h1 className="font-bold	">#{table.number}</h1>
+                <h2 > Check in Time:  {table.active ? table.checkInTime : "00:00"} </h2>
             </div>
+
         </>
     )
 }
