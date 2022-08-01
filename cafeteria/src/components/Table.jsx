edc0f1573b3f634505abd5055b67014a00dd7607
Trinidad Medina <trinidad.medina.silva@gmail.com>
2022-07-31 22:30:58 -0400

    resolve-conflicts-merge

--- cafeteria/src/components/Table.jsx
+++ cafeteria/src/components/Table.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
-export const Table = ({ table }) => {
+export const Table = ({ table, showInfoTable, selectTable }) => {
+
+    const handleSelectedTable = (table) => {
+        selectTable(table);
+        showInfoTable();
+    }
+
     return (
         table.number && <>
-            <div className={table.active ? " p-4 max-w-sm mx-auto bg-cyan-600	 shadow-lg rounded-lg overflow-hidden" : " p-4 max-w-sm mx-auto bg-gray-300	 shadow-lg rounded-lg overflow-hidden"}>
+            <div onClick={() => { handleSelectedTable(table) }} className={table.active ? " p-4 max-w-sm mx-auto bg-cyan-600	 shadow-lg rounded-lg overflow-hidden" : " p-4 max-w-sm mx-auto bg-gray-300	 shadow-lg rounded-lg overflow-hidden"}>
                 <h1 className="font-bold	">#{table.number}</h1>
+                <h3 className="font-bold	">Active:{table.active.toString()}</h3>
                 <h2 > Check in Time:  {table.active ? table.checkInTime : "00:00"} </h2>
             </div>
 
         </>
     )
 }
-
-
-{/* <div style={{ border: "solid 2px black" }}>
-    {!tables ? null : tables.map((table) => ())}
-</div> */}
--- cafeteria/src/components/TableInfo.jsx
+++ cafeteria/src/components/TableInfo.jsx
@@ -1,26 +1,29 @@
 import React, { useState } from 'react'
 
-export const TableInfo = ({ isShown, closeTableInfo, selectedTable }) => {
-
+export const TableInfo = ({ isShown, closeTableInfo, selectedTable, openMenu, activateTable }) => {
+    const { number, bill, active, order, checkInTime } = selectedTable;
     if (isShown) {
-        return (<>
-
-            <button onClick={closeTableInfo}> x </button>
-            <div style={{ border: "2px solid black" }}>TableInfo
-                <div> Table: #{selectedTable.number} <br />
-                    Bill: $ {selectedTable.bill} <br />
-                    Time:{selectedTable.checkInTime} <br />
+        return (
+            <section className='place-content-center flex flex-col p-8 w-96 py-4 px-3 my-4 max-w-sm mx-auto bg-white shadow-lg rounded-lg '>
+                <div className='place-content-center justify-between flex flex-row-reverse'>
+                    <button className=' bg-gray-500    text-white font-bold py-1 px-2  rounded' type="button" onClick={() => { closeTableInfo(isShown) }}> X </button>
+                    <h1 className='font-bold text-xl'>Table {number}</h1>
                 </div>
-                {/* {tables.map((table) => (<>
-                    <h1 style={{ color: "red" }} key={table.number}> Table number: {table.number} </h1>
-                    <h2 style={{ color: "red" }}> Orders: {!table.orders.length ? "No Orders" : table.orders.map((orders) => (<> <p>Orden:{orders.idOrder}</p></>))} </h2>
-                    <h3 style={{ color: "red" }}>Bill: {table.bill}</h3>
-                </>))} */}
-
-            </div>
-        </>
+                <article>
+                    <ul>
+                        <li>Bill: $ {bill}</li>
+                        <li>Time:{active && checkInTime}</li>
+                        <li>Order:{order}</li>
+                        <li>active:{active.toString()}</li>
+                    </ul>
+                    {active ?
+                        <button className=' bg-gray-500   place-content-center text-white font-bold py-2 px-4  rounded' type="button" onClick={() => { console.log(selectedTable) }}> CheckOut #{number}</button> :
+                        <button className=' bg-gray-500  place-content-center text-white font-bold py-2 px-4  rounded' type="button" onClick={() => { console.log(selectedTable), activateTable() }}> CheckIn #{number}</button>}
+                    <button className=' bg-gray-500   place-content-center text-white font-bold py-2 px-4  rounded' type="button" onClick={() => { openMenu() }}> Add product </button>
+                </article>
+            </section>
         )
-    } else { return <></> }
+    }
 
 }
 
