770636169d4f9c385bcfa2165c50a94c010acbc1
Trinidad Medina <trinidad.medina.silva@gmail.com>
2022-08-17 09:20:41 -0400

    error-tipeo

--- cafeteria/src/views/Diner.jsx
+++ cafeteria/src/views/Diner.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useContext } from 'react'
 import { Clock } from '../components/Clock'
 import { Order } from '../components/Order'
-import { OrdersContext } from '../context/ordersContext.jsx'
+import { OrdersContext } from '../context/OrdersContext.jsx'
 import { TablesContext } from '../context/TablesContext.jsx'
 import { useNavigate } from "react-router-dom";
 import { ButtonSignOut } from '../components/ButtonSignOut'
